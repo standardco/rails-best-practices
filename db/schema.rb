8d38a5910b2a75d5f43fc1ae16dcaa3aca22df80
Jonathan Nesbitt <jon.nesbitt@gmail.com>
2015-06-24 08:39:42 -0400

    Added image attribute to the Image model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624042735) do
+ActiveRecord::Schema.define(version: 20150624123904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150624042735) do
     t.integer  "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   add_index "images", ["product_id"], name: "index_images_on_product_id", using: :btree
