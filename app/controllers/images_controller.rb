04357c19676ce5d8e48c658bd2cd07fdb0077263
Jonathan Nesbitt <jon.nesbitt@gmail.com>
2015-06-24 08:33:21 -0400

    Fixed image delete

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -55,9 +55,10 @@ class ImagesController < ApplicationController
   # DELETE /images/1
   # DELETE /images/1.json
   def destroy
+    @product = @image.product
     @image.destroy
     respond_to do |format|
-      format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
+      format.html { redirect_to @product, notice: 'Image was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
