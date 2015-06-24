247aeeff3b72cc2358c0b5deaad8c4149a5d9a5e
Jonathan Nesbitt <jon.nesbitt@gmail.com>
2015-06-24 08:59:45 -0400

    Added image attribute to permitted params

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -75,6 +75,6 @@ class ImagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-      params.require(:image).permit(:name, :product_id)
+      params.require(:image).permit(:name, :product_id, :image)
     end
 end
