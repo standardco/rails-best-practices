4e43fcbdaa75f9e4c4c9ecd1426351c0b009ba46
Jonathan Nesbitt <jon.nesbitt@gmail.com>
2015-06-24 09:32:14 -0400

    Redirecting to the product page after adding or updating an image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -29,7 +29,7 @@ class ImagesController < ApplicationController
 
     respond_to do |format|
       if @image.save
-        format.html { redirect_to @image, notice: 'Image was successfully created.' }
+        format.html { redirect_to @image.product, notice: 'Image was successfully created.' }
         format.json { render :show, status: :created, location: @image }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class ImagesController < ApplicationController
   def update
     respond_to do |format|
       if @image.update(image_params)
-        format.html { redirect_to @image, notice: 'Image was successfully updated.' }
+        format.html { redirect_to @image.product, notice: 'Image was successfully updated.' }
         format.json { render :show, status: :ok, location: @image }
       else
         format.html { render :edit }
