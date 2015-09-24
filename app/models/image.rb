46faf3ca5bd39e9fb57037594c0caf5489834938
Jonathan Nesbitt <jon.nesbitt@gmail.com>
2015-09-24 17:01:08 -0400

    Added presence validation for the image attachment on the Image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,6 @@
 class Image < ActiveRecord::Base
   belongs_to :product
-  has_attached_file :image, styles: {
-    thumb: '350x350#'
-  }
+  has_attached_file :image, styles: { thumb: '350x350#' }
+  validates_attachment_presence :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
