08edd865c5d2b2bb433ad21737f6b1c3023b8695
Jonathan Nesbitt <jon.nesbitt@gmail.com>
2015-09-24 14:39:49 -0400

    Removed 'image' column from the Image model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624123904) do
+ActiveRecord::Schema.define(version: 20150924183831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20150624123904) do
     t.integer  "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "image"
   end
 
   add_index "images", ["product_id"], name: "index_images_on_product_id", using: :btree
