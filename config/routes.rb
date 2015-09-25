e7675c909f66b4bbd6ec7d19cd520ddd5b70fc5d
Jonathan Nesbitt <jon.nesbitt@gmail.com>
2015-09-24 21:40:15 -0400

    Added ApiContraints class, and configured the Product API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
 
-  root 'home#index'
+  root to: 'products#index'
   
   get 'messaging', to: 'messaging#stream_message'
+  
+  namespace :api, defaults: { format: 'json' } do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: :true) do
+      resources :products
+    end
+  end
   
   get 'stream_products', to: 'products#stream_products'
   resources :products do
