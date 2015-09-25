de43401b131f0c9e36c8bb857c711b507196bb9b
Jonathan Nesbitt <jon.nesbitt@gmail.com>
2015-09-24 22:15:09 -0400

    Fixed strong params in the API ProductsController

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -28,7 +28,7 @@ module Api
       private
         # Never trust parameters from the scary internet, only allow the white list through.
         def product_params
-          params.require(:product).permit(:name, :story, :image, :latitude, :longitude)
+          params.require(:product).permit(:name)
         end
     end
   end
